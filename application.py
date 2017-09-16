be28f8666d439fa7067dd31fe6c0f6e684100b68
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-09-16 21:58:05 +0300

    application.run() ei pitäisi tarvita aws:ssä

--- application.py
+++ application.py
@@ -2,8 +2,6 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-application = create_app(config_name)
-
 if __name__ == '__main__':
-    application.run()
+    config_name = os.getenv('FLASK_CONFIG')
+    application = create_app(config_name)

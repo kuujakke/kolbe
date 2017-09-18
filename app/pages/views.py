2575f1d3a67a6472c667dd82e286277f240b29ab
kuujakke <jakke.kuukkanen@helsinki.fi>
2017-09-18 23:10:10 +0300

    Lisätty sisällön tuova muuttuja

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from . import pages
+from app import models
 
 @pages.route('/')
 def index():
-    return render_template('pages/index.html', title="Pages")
+
+    return render_template('pages/index.html',
+                           title="Pages",
+                           pages=models.get_pages())

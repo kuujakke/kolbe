4baeff05ca638425c5a5fe479a40f3b60c73fb26
kuujakke <jakke.kuukkanen@helsinki.fi>
2017-09-23 21:19:17 +0300

    Lorem Ipsumit testidataksi

--- app/database/add_test_data.sql
+++ app/database/add_test_data.sql
@@ -1,4 +1,60 @@
 INSERT INTO "users" (email, password) VALUES ('testi@helsinki.fi', 'salaisuus');
-INSERT INTO "pages" (user_id, content) VALUES (1, '# Testisivu 1 \n Tämä on *testi*');
 INSERT INTO "comments" (page_id, user_id, content) VALUES (1, 1, 'Testikommentti **painokkaasti** ilmaistuna');
 INSERT INTO "tags" (page_id, content) VALUES (1, 'Testiaiheentunnistin');
+
+INSERT INTO "pages" (user_id, content) VALUES (1, '# Circumspicit fuit aeolis pater
+
+## Tunc Titania
+
+Lorem markdownum vocalis ingemuit unguibus, velamine hominum cetera. Mirantia
+cognoscere mugire sideribus curruque se possem palmas fletumque umbras *mersit
+diroque* radices; cadunt membris domum. Terram congerie.
+
+## Illa mea parte quae dum illuc tersis
+
+Quam venit sequitur felix sequenti, nusquam tenet suo pelagi loco haud. Infernas
+missae amorem! Possumus has caelestes mihi mitra
+circumtulit tectis umbras reddidit, tulerit alite **sed**, nec crura. Simul fuga
+inmittor tegi vivere perque utrimque. Turbine temptantes.
+
+## Horrifer foret me vincant Mysum sinu aequora
+
+Nil *iamque*, divitis Sol veniret defuit magnumque nocent clamor flamma? Amplexa
+ita urbem colla, filius tumet in vitare Boeotia fallitur flecti.
+
+> *Quae spatium*, lux alis quibus, **est**! Quodsi tum litore, et multa torsit,
+> heros mediamque tellus e crearat Dianae.
+
+## Exsiluere poenaque fuerant canibus feruntur
+
+Sustinuere libido, [si donec maerenti](http://www.haec.io/) postquam aequoreo.
+Possideat *fudi fieri* ait exanimis carpebam urnae caelumque, quem rari dies
+dicere lotos.
+
+    computer.skinStateMinimize *= whitelist.abendUnitRup(tag, extension);
+    var secondaryVdsl = oasisTelnetIscsi - textToggleJsp(offline, version_ipad,
+            honeypot_motherboard) - -5;
+    root_scanner_spam = 1 * 21 + input;
+
+## Requiras satis
+
+Cantibus ac ramis: dare sic, semper **sole unus texit**: ut domo Peleus, secum
+miracula; sine! Erat currus arreptum, cunctis gravitate dextrum siqua, nunc
+fore. Sic **et** ecce sibi labores adlevat Melantho nefas coniunx stratum
+patruo, at. Quoque redeunt queritur dominae vidit summa narravere et iamque Hoc
+opem Samon, coepta morati gradu. Veri solverat Clymeni monitis tantoque nubemque
+tanta Pagasaea, studioque **urbis**, in fecit missus, ut navem?
+
+Sub sibi tamen territa sed quod exul per condebat nitentem; in videt pennis.
+Cognoscenti Echo remoraminaque terras sollertius dixi cunctantem dolores,
+sagittis terras.');
+
+INSERT INTO "pages" (user_id, content) VALUES (1, '# Samuel L. Ipsum
+
+You think water moves *fast*? You should see **ice**. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don`t know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I`m breaking now. We said we`d say it was the snow that killed the other two, but it wasn`t. _Nature is lethal but it doesn`t hold a candle to man_.
+
+Well, the way they make shows is, they make one show. That show`s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they`re going to make more shows. Some pilots get picked and become television programs. Some don`t, become nothing. She starred in one of the ones that became nothing.
+
+Now that there is the _Tec-9_, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they`re actually proud of that shit. 
+
+Your bones don`t break, mine do. That`s clear. Your cells react to bacteria and viruses differently than mine. You don`t get sick, I do. That`s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We`re on the same curve, just on opposite ends. ');

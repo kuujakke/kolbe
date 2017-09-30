a03f9635e94746476dbb3c251af6b884b10ff8bf
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-09-30 14:24:00 +0300

    Lisätty käynnistysskripti staattisilla ympäristömuuttujilla

---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+RDS_HOSTNAME=kolbe-db.c7jodkrsqzdr.eu-central-1.rds.amazonaws.com RDS_PASSWORD=W7S+13SRcdfcOoMXAi7SlBrjih3eAyfj RDS_PORT=5432 FLASK_CONFIG=development RDS_DB_NAME=kolbe SECRET_KEY=R4/OyNkVoYWZRibbSqQoSZlR6xqLi9Qw RDS_USERNAME=kuujakke python application.py

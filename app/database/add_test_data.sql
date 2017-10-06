23e0f656a9f092b138b497aebe5d59233d51f1ad
kuujakke <jakke.kuukkanen@helsinki.fi>
2017-10-06 22:30:01 +0300

    Tehty testidatasta mielenkiintoisempaa

--- app/database/add_test_data.sql
+++ app/database/add_test_data.sql
@@ -1,62 +1,71 @@
 INSERT INTO "users" (email, password) VALUES ('testi@helsinki.fi', 'salaisuus');
 
-INSERT INTO "pages" (user_id, content) VALUES (1, '# Circumspicit fuit aeolis pater
+INSERT INTO "users" (email, password) VALUES ('pelikaani@turbiini.fi', 'wanha');
 
-## Tunc Titania
+INSERT INTO "pages" (user_id, content) VALUES (2, '# Valikoiva oli isä Aeoluses
+## Sitten Titan
+Lorem markdown laulu teki kynnet ja kattavat loput miehiä. Kämmenten Hänen tuskan itkien autosta varjossa tähdet, vaikerrus, tietää voisin tehdä, että ihmetteli itsekin , upposi ja tuli sen jalka; lankea kotitalouden jäsenille. Jauhaa.
 
-Lorem markdownum vocalis ingemuit unguibus, velamine hominum cetera. Mirantia
-cognoscere mugire sideribus curruque se possem palmas fletumque umbras *mersit
-diroque* radices; cadunt membris domum. Terram congerie.
+## Pointtini on, että vaikka tersis
+Kuinka onnellinen tästä seuraa, että seuraavana päivänä hän tuli, hän ei voi koskaan pitää omaa merelle ja ei voi olla. Helvetti oli rakkautta! Hän antoi minulle renkaan, varjot kattojen selestisen, on tehnyt noin heidän kanssaan, Pystymme mies, läpäisi lintu, mutta ei jalat. Samalla elää lennon, minä lähetän katettava ja se, joka ottaa vastaan. Testata turbiini.
 
-## Illa mea parte quae dum illuc tersis
+## En anna sen ilmi Persianlahden vesillä
+Mitään, ja nyt, huuto rikkaan on liekki auringon ovat haitallisia tulemista ei ottanut pois, ja että suuri? Joten kaupunki on omaksunut niska ja poika muuttuu sotiin välttämään virheitä taipunut.
 
-Quam venit sequitur felix sequenti, nusquam tenet suo pelagi loco haud. Infernas
-missae amorem! Possumus has caelestes mihi mitra
-circumtulit tectis umbras reddidit, tulerit alite **sed**, nec crura. Simul fuga
-inmittor tegi vivere perque utrimque. Turbine temptantes.
+Mitä tilaa, valoa siivet, joiden kanssa, se on! Mutta jos näin on rannalla, ja kärsimän paljon iskuja, sankari keskialueella, hän oli luonut maan ulos temppelistä Diana.
+## Hyppy rangaistus, suoritetaan koiria
+Jatka halu, jos sureva ennen mereen. Hallussaan irtoa tehtävä, sanoo elottoman ruumiin oli tarttua uurnan ja taivas, päivä joista pienet puolueet ja sanoa, kun se pesi ne.
 
-## Horrifer foret me vincant Mysum sinu aequora
+## Vaatii tarpeeksi
+Ja kappaleita oksat antaa, ne ovat aina aurinkoa, yksi se oli peitetty, jotta talon Peleus, jossa ihmeensä; Ilman! Takavarikoimalla auton, okei paino, jos sellainen olisi nyt. Näin, ja katso, vaimo rikoksen itse kaikki ansa Hän nosti melanthos sohvalla setänsä, ja. Kun nainen näki ongelman ja kertoi summan palaa osoituksena tästä avusta, ja tasoristeyksen. Varoitukset ja taivaan pienemmän rakkaus niin paljon oli jo rikki Clymenin totuus, ja halu kaupungin, että hän teki, hänet lähetettiin, jotta laiva?
 
-Nil *iamque*, divitis Sol veniret defuit magnumque nocent clamor flamma? Amplexa
-ita urbem colla, filius tumet in vitare Boeotia fallitur flecti.
-
-> *Quae spatium*, lux alis quibus, **est**! Quodsi tum litore, et multa torsit,
-> heros mediamque tellus e crearat Dianae.
-
-## Exsiluere poenaque fuerant canibus feruntur
-
-Sustinuere libido, [si donec maerenti](http://www.haec.io/) postquam aequoreo.
-Possideat *fudi fieri* ait exanimis carpebam urnae caelumque, quem rari dies
-dicere lotos.
-
-    computer.skinStateMinimize *= whitelist.abendUnitRup(tag, extension);
-    var secondaryVdsl = oasisTelnetIscsi - textToggleJsp(offline, version_ipad,
-            honeypot_motherboard) - -5;
-    root_scanner_spam = 1 * 21 + input;
-
-## Requiras satis
-
-Cantibus ac ramis: dare sic, semper **sole unus texit**: ut domo Peleus, secum
-miracula; sine! Erat currus arreptum, cunctis gravitate dextrum siqua, nunc
-fore. Sic **et** ecce sibi labores adlevat Melantho nefas coniunx stratum
-patruo, at. Quoque redeunt queritur dominae vidit summa narravere et iamque Hoc
-opem Samon, coepta morati gradu. Veri solverat Clymeni monitis tantoque nubemque
-tanta Pagasaea, studioque **urbis**, in fecit missus, ut navem?
-
-Sub sibi tamen territa sed quod exul per condebat nitentem; in videt pennis.
-Cognoscenti Echo remoraminaque terras sollertius dixi cunctantem dolores,
-sagittis terras.');
+Jonka avulla maanpakoon, ei kuitenkaan luo ne itselleen, mutta nykyisessä on kauhuissaan. Hän näkee siivet. Sanoin, epäröivät toimia piiriä kaiku kivut tietävä remoraminaquen mailla mailla nuolia.'
+);
 
 INSERT INTO "pages" (user_id, content) VALUES (1, '# Samuel L. Ipsum
+Luuletko, että vesi liikkuu nopeasti? Sinun pitäisi nähdä jäätä. Se liikkuu kuin siinä mielessä. Kuten se tietää, se tappoi maailman kerran ja sai maistaa murhaa. Vyöhykkeeltä jälkeen kesti viikkoa kiivetä. Nyt en tiedä tarkkaan, kun käännyimme toisiimme, mutta tiedän, että seitsemän meistä selviytyi liukumäestä ... ja vain viisi sain sen ulos. Nyt teimme valan, että Imurtaudu nyt. Sanoimme sanoa, että lumi tappoi kaksi muuta, mutta se ei ollut. Luonto on tappava, mutta se ei pidä kynttilää ihmiselle.
 
-You think water moves *fast*? You should see **ice**. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don`t know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I`m breaking now. We said we`d say it was the snow that killed the other two, but it wasn`t. _Nature is lethal but it doesn`t hold a candle to man_.
+Ei, miten he esittävät näyttelyt, hän tekee yhden näytelmän. Tämä näyttää kutsuttu pilotti. Sitten he osoittavat tämän näytön ihmisille, jotka tekevät esityksiä, ja tämän yhden näytelmän vahvuudesta päättävät, jos he ovat enemmän esityksiä. Jotkut lentäjät hakevat ja tulevat televisio-ohjelmia. Jotkut eivät, ei ole mitään. Hän esiintyi yhdessä niistä, joista ei tullut mitään.
 
-Well, the way they make shows is, they make one show. That show`s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they`re going to make more shows. Some pilots get picked and become television programs. Some don`t, become nothing. She starred in one of the ones that became nothing.
+Nyt kun Tec-9 on Etelä-Miamissa höyrytetty ruiskupistooli. Tämä ase mainostetaan suosituimpaan aseena amerikkalaiseen rikollisuuteen. Uskotko tuo paska? Se itse asiassa sanoo sen pienessä kirjassa, joka tulee siihen: suosituin ase amerikkalaiseen rikollisuuteen. Kuten he ovat todella ylpeitä tuosta paskasta.
 
-Now that there is the _Tec-9_, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they`re actually proud of that shit. 
+Luut eivät riko, minun tekevät. Sehän on selvä. Solut reagoi bakteereihin ja viruksiin eri tavalla kuin minun. Et saa sairastua, minä. Se on myöskään selkeä. Mutta jostain syystä te ja minä reagoimme täsmälleen samaan tapaan veden kanssa. Me nielemme sen liian nopeasti, minä kuristelemme. Saamme jotain keuhkoihimme, hukkumme. Kuitenkin epärealistinen se tuntuu, olemme yhteydessä, sinä ja minä. Meillä on sama käyrä, vastakkaisissa päissä.'
+);
 
-Your bones don`t break, mine do. That`s clear. Your cells react to bacteria and viruses differently than mine. You don`t get sick, I do. That`s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We`re on the same curve, just on opposite ends. ');
+INSERT INTO "comments" (page_id, user_id, content) VALUES (1, 1, '## Kaada keskellä kannen ja kuusi hevosta
+Ihastui viettelys valtakuntien Frayn avulla liioiteltu yksi hänellä oli rautavaunua, Laertes, oli tietäen toiset. Äiti lähti, viime aikoina? Kun annetaan metsässä ja ennakkotietoa kölin kansi.
 
-INSERT INTO "comments" (page_id, user_id, content) VALUES (1, 1, 'Testikommentti **painokkaasti** ilmaistuna');
+1. Profeetallisella sanat kiipeilijöiden
+2. Jos tekijä, on viipymättä lähetettävä sammuttamaan rohkea käsite
+3. Vanhempi nyt uppoamassa valtaistuimelle
+');
+
+INSERT INTO "comments" (page_id, user_id, content) VALUES (1, 2, '## Ja löytää se alla kosketusohjaus saaliseläinten
+Lorem markdowns on lämmin ja sitten kasvaa sekä Aiakos pojanpoika oli leiriytynyt elää, ja soraa, ja te ette *hyväksy*, koska muodot niistä Hänen kehonsa kanssa! Ja uudella repivät pois vastustavat kaikkia, ja osallistua siihen olisi niin paljon, ja nyt se on toinen kerta, hän lupaa rajoittava. Olisi suolla kyliä yksin, suurin pudotus ylhäältä. Kaksoistemppelin vihaa ja mieheni on aina diureetti, kevyempi ja tiuskaisi nähdä hänen selkäänsä. Kuitenkin hoito antaa ja kierrosta.
+
+Ennaltaehkäisy ja myönsi, että perussopimusten; suukkoja mahtavia jotka näkivät niin tumma, ominaisuus, ne minulle. Hän oli siellä; annettavaa ihmispelko, Niin, mies oman sykkeen rintaansa? Kiviä, jotka haluavat asuttu, riittää tapaan yrtit, enemmän julma kuin vaiheilla varkaudesta. Telineet ovat esti nopeasti oikeaan . Mitä hukkuivat, katsellen, ja kun hän, luisto, onko hän saa se on turvonnut syntyi kesken: kanssa syytä.
+');
+
+INSERT INTO "comments" (page_id, user_id, content) VALUES (2, 2, '## Onko hän palaa meidän siirtyy tummaa kaivoksesta
+
+Rukoukset Priamoksena, jolle värin nimen, joka on aseet väännetty päässä sitkeästi samoja asioita teistä, rakkaimmat.
+Ja sanoin? Ääni niin monet säädösten käämi on kärsivällisyyttä Achilleen, joka oli yhtäkkiä Iphides.
+Rakkaus, sati. Palaemon mitään roolia siinä.
+
+## Mielissä jotka sinä
+
+Sheen kanssa pyhä, todellinen ja ohjasi hänet, vaientaa. Ja
+Jupiter! Ambrosialla haava pestiin. Tiettyjä tulipalot, ensisijaisesti käsittää.
+Piispa villi, taistella mereen. Ilman kasvoni ja sitten kun?
+');
+
+
+INSERT INTO "comments" (page_id, user_id, content) VALUES (2, 1, '## Hyvää liekki
+
+Ja se on aina ollut. Ajat nosti hänelle paljon viisaus, hän pystytti fruticosan tavalla, sinun on hämmästynyt, ja
+osa voi vielä olla hyväksyttävää, koko polku? Ylöspäin toisessa muodossa, kuten
+aamuisin hänen luita aura rikkaus.
+Hän sanoi: Hän otti toisistaan ​​poikkeavissa olosuhteissa: Raskas, miksi on pelko siitä, että myötäjäiset.
+Äiti.');
 
 INSERT INTO "tags" (page_id, content) VALUES (1, 'Testiaiheentunnistin');
